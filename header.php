70640190581263bfe4e04440120557db4b82f8dc
Catsquid17 <catsquid.17@gmail.com>
2025-07-16 20:55:06 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,35 @@
+<header class="p-3 mb-3 border-bottom">
+  <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+    <div class="title">
+      <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-gitlab" viewBox="0 0 16 16">
+          <path
+            d="m15.734 6.1-.022-.058L13.534.358a.57.57 0 0 0-.563-.356.6.6 0 0 0-.328.122.6.6 0 0 0-.193.294l-1.47 4.499H5.025l-1.47-4.5A.572.572 0 0 0 2.47.358L.289 6.04l-.022.057A4.044 4.044 0 0 0 1.61 10.77l.007.006.02.014 3.318 2.485 1.64 1.242 1 .755a.67.67 0 0 0 .814 0l1-.755 1.64-1.242 3.338-2.5.009-.007a4.05 4.05 0 0 0 1.34-4.668Z"
+          />
+        </svg>
+        Catsquid's Stuff
+      </a>
+    </div>
+    <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">My Mods</a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="all-mods.html">All Mods</a></li>
+        </ul>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Mod Creation</a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Modding Guide</a></li>
+          <li><a class="dropdown-item" href="#">Modding Tools</a></li>
+        </ul>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Misc</a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Kitten Pelt Previewer</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</header>
